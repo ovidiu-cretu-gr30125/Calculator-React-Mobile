ff1be3392e12cf03b752efcc9cf295e7c3745e20
ovidiu-cretu-gr30125 <cretuovidiudaniel@yahoo.com>
2020-08-17 21:50:56 +0300

    Updates

---
+++ precache-manifest.6ad5e12acbc49bfee314376f38f3dded.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a265062fb54f3142a1a73dc22dcffa8",
+    "url": "/Calculator-React-Mobile/index.html"
+  },
+  {
+    "revision": "90186b70c58918d11d5c",
+    "url": "/Calculator-React-Mobile/static/css/main.f99ba43c.chunk.css"
+  },
+  {
+    "revision": "114bbaea14242877659e",
+    "url": "/Calculator-React-Mobile/static/js/2.6912228e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Calculator-React-Mobile/static/js/2.6912228e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90186b70c58918d11d5c",
+    "url": "/Calculator-React-Mobile/static/js/main.de7f798e.chunk.js"
+  },
+  {
+    "revision": "0d2425bc17fd3e607404",
+    "url": "/Calculator-React-Mobile/static/js/runtime-main.34df7dfb.js"
+  },
+  {
+    "revision": "dd857e7b06acf30c32e43350fcd82ca3",
+    "url": "/Calculator-React-Mobile/static/media/ClipartKey_2783339.dd857e7b.png"
+  },
+  {
+    "revision": "c831d3934deff2c1ca0c3d642eb39ba6",
+    "url": "/Calculator-React-Mobile/static/media/PngItem_5331595.c831d393.png"
+  },
+  {
+    "revision": "c32d16ecc826e0d3ffd2fc97fe70afa3",
+    "url": "/Calculator-React-Mobile/static/media/pngwing.com.c32d16ec.png"
+  },
+  {
+    "revision": "2e770dc7ec2d7fcfb8e62378cc655cea",
+    "url": "/Calculator-React-Mobile/static/media/unnamed.2e770dc7.png"
+  }
+]);
